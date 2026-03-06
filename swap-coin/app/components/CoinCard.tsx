89287d3b71a5060a0b500f6f95c6e55fe8b4aff8
Amogh-007-Rin <amoghdath233@gmail.com>
2026-03-06 05:03:32 +0000

    Added crypto-coins-icons

--- swap-coin/app/components/CoinCard.tsx
+++ swap-coin/app/components/CoinCard.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, Image, Text, View, ActivityIndicator } from 'react-native';
-import { getCoinData } from '../../scripts/getCoinData';
+import { Coin, getCoinData } from '../../scripts/getCoinData';
 
 const CoinCard = () => {
-  const [coins, setCoins] = useState([]);
+  const [coins, setCoins] = useState<Coin[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -22,9 +22,18 @@ const CoinCard = () => {
       data={coins}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
-          <Image source={{ uri: item.image }} style={{ width: 30, height: 30, marginRight: 10 }} />
-          <Text>{item.name} ({item.symbol.toUpperCase()})</Text>
+        <View className='flex-row justify-start items-center w-96 h-16 bg-[#29292b] m-1 rounded-2xl'>
+          <View className='w-1/4 h-full bg-black flex justify-center items-center'>
+            <Image source={{ uri: item.image }} className='w-12 h-12 rounded-full' />
+          </View>
+          <View className='flex justify-center items-center bg-blue-400 w-1/4 h-full'>
+            <Text className='text-white'>{item.name}</Text>
+            <Text className='text-white my-2'>0 {item.symbol.toUpperCase()}</Text>
+          </View>
+          <View className='w-14 h-full flex-col justify-center items-center rounded-2xl bg-green-400 mx-32'>
+            <Text className='text-black text-xl'>$0.00</Text>
+            <Text className='text-black -my-1'>$0.00</Text>
+          </View>
         </View>
       )}
     />
